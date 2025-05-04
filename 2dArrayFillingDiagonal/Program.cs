aafac28cb8920ab1e9d9102050461b099955822e
Yordan Yordanov <mixdata34@gmail.com>
2025-05-04 18:24:35 +0300

    Add project files.

---
+++ 2dArrayFillingDiagonal/Program.cs
@@ -0,0 +1,61 @@
+﻿/// <summary>
+///     This is Exercise 1 from page 45 (The Matrix section) of
+///     the Programming = ++ Algorithms book by Preslav Nakov and Panayot Dobrikov
+/// </summary>
+/// <see href="https://programirane.org/"/>
+
+
+namespace _2dArrayFillingDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n = 5;
+            uint[,] matrix = new uint[n, n];
+            FillMatrix(matrix, n);
+            PrintMatrix(matrix, n);
+        }
+        // This method is not part of the exercise but it is used for testing purposes
+        static void PrintMatrix(uint[,] matrix, uint n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{matrix[i, j],3} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void FillMatrix(uint[,] matrix, uint n)
+        {
+            uint counter = 1;
+            // 0 diagonal
+            for (uint i = 0; i < n; i++)
+            {
+                matrix[i, i] = 0;
+            }
+            // below diagonal
+            uint r = 1;
+            for (int col = 0; col < n; col++)
+            {
+                for (uint row = r; row < n; row++)
+                {
+                    matrix[row, col] = counter++;
+                }
+                r++;
+            }
+            // above diagonal
+            r = n - 2;
+            for (uint col = n - 1; col > 0; col--)
+            {
+                for (uint row = r; row < n; row--)
+                {
+                    matrix[row, col] = counter++;
+                }
+                r--;
+            }
+        }
+    }
+}
---
+++ 2dArrayFillingSnake/Program.cs
@@ -0,0 +1,72 @@
+﻿/// <summary>
+///     This is Exercise 2 from page 45 (Chapter 1, The Matrix section) of
+///     the Programming = ++ Algorithms book by Preslav Nakov and Panayot Dobrikov
+/// </summary>
+/// <see href="https://programirane.org/"/>
+
+namespace _2dArrayFillingSnake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the matrix: ");
+            int n = int.Parse(Console.ReadLine());
+            uint[,] matrix = new uint[n, n];
+            FillMatrix(matrix, n);
+            PrintMatrix(matrix, n);
+        }
+        // This method is not part of the exercise but it is used for testing purposes
+        static void PrintMatrix(uint[,] matrix, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{matrix[i, j],3} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void FillMatrix(uint[,] matrix, int n)
+        {
+            int steps = n;
+            uint count = 1;
+            int currRow = 0;
+            int currCol = 0;
+            while (steps > 0)
+            {
+                //down
+                for(int row = currRow, v = 0; v < steps; row++, v++)
+                {
+                    currRow = row;
+                    matrix[currRow, currCol] = count++;
+                }
+                // right
+                currCol++;
+                steps--;
+                for(int col = currCol, v = 0; v < steps; col++, v++)
+                {
+                    currCol = col;
+                    matrix[currRow, currCol] = count++;
+                }
+                // up
+                currRow--;
+                for (int row = currRow, v = 0; v < steps; row--, v++)
+                {
+                    currRow = row;
+                    matrix[currRow, currCol] = count++;
+                }
+                // left
+                currCol--;
+                steps--;
+                for (int col = currCol, v = 0; v < steps; col--, v++)
+                {
+                    currCol = col;
+                    matrix[currRow, currCol] = count++;
+                }
+                currRow++;
+            }
+        }
+    }
+}
