df3788f4d1ea100defd0dc6f002858963ebf0586
Yordan Yordanov <mixdata34@gmail.com>
2025-05-06 20:07:41 +0300

    Added a IsNumberPrime, accidently named prima, which still works ;)

---
+++ IsNumberPrima/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace IsNumberPrima
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your number!");
+            uint n = uint.Parse(Console.ReadLine());
+            if (IsPrime(n))
+            {
+                Console.WriteLine("The number is prime!");
+            }
+            else
+            {
+                Console.WriteLine("The number is not prime!");
+            }
+
+        }
+        static bool IsPrime(uint n)
+        {
+            uint i = 2;
+            if (n == 2) return true;
+            while(i <= Math.Sqrt(n))
+            {
+                if(n % i == 0) return false;
+                i++;
+            }
+            return true;
+        }
+    }
+}
