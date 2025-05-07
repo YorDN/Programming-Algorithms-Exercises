ea49f6230439309176d5a78d0a9b626c9479f9ae
Yordan Yordanov <mixdata34@gmail.com>
2025-05-07 20:13:56 +0300

    Added PrimeFactorization

---
+++ PrimeFactorization/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PrimeFactorization
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int i, j, how;
+            Console.Write($"{n} = ");
+            i = 1;
+            while(n != 1)
+            {
+                i++;
+                how = 0;
+                while(n % i == 0)
+                {
+                    how++;
+                    n /= i;
+                }
+                for(j = 0; j < how; j++)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
